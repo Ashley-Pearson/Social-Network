1399b110972c22df48f201bed53c95e87b392773
Ashley Pearson <ashleypearson@Ashleys-MacBook-Air.local>
2023-01-28 16:46:18 -0700

    add new user schema

--- models/user.js
+++ models/user.js
@@ -1,1 +1,26 @@
-// Schemas
+const { Schema, model } = require('mongoose');
+
+// Schema to create a new user 
+const userSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+            trimmed: true,
+        },
+        emailAddress: {
+            type: String,
+            required: true,
+            validate: {
+                validator: function (v) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+                },
+                message: 'Not a valid email address'
+
+            }
+        },
+
+    });
+
+    module.exports = User;
